710f92219a0af0042f4163fc995c5599d4b958d2
Antlin23 <anton.jumkil@gmail.com>
2025-04-01 23:27:38 +0200

    day 11 part 1 done

--- AdventOfCode/Day11.cs
+++ AdventOfCode/Day11.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace AdventOfCode {
@@ -14,55 +15,91 @@ namespace AdventOfCode {
             //array with numbers
             string[] inputSplitted = input.Split(' ');
             //list with a list with numbers
-            List<List<int>> line = new List<List<int>>();
+            List<List<long>> line = new List<List<long>>();
 
             foreach (string inputSplit in inputSplitted) {
-                List<int> number = new List<int>();
+                List<long> number = new List<long>();
                 for (var i = 0; i < inputSplit.Length; i++)
                 {
-                    number.Add(int.Parse(inputSplit[i].ToString()));
+                    number.Add(long.Parse(inputSplit[i].ToString()));
                 }
                 line.Add(number);
             }
 
-            for(var i = 0; i < line.Count; i++)
+            for (int k = 0; k < 75; k++)
             {
-                //changes all 0 to 1
-                if (line[i].Count == 1 && line[i][0] == 0)
+                for (var i = 0; i < line.Count; i++)
                 {
-                    line[i][0] = 1;
-                }
-
-                //checks if its an even amount of numbers
-                else if (line[i].Count() % 2 == 0)
-                {
-                   // Console.WriteLine(line[i][0]);
-                }
-
-                //multiply by 2024
-                else
-                {
-                    int calcedNum = int.Parse(string.Join("", line[i])) * 2024;
-
-                    List<int> newNum = new List<int>();
-
-                    string stringed = calcedNum.ToString();
-
-                    for (var j = 0; j < stringed.Length; j++)
+                    //changes all 0 to 1
+                    if (line[i].Count == 1 && line[i][0] == 0)
                     {
-                        newNum.Add(int.Parse(stringed[j].ToString()));
+                        line[i][0] = 1;
                     }
 
-                    line[i] = newNum;
+                    //checks if its an even amount of numbers
+                    else if (line[i].Count() % 2 == 0)
+                    {
+                        List<long> firstHalfOfList = new List<long>();
+
+                        List<long> newList = new List<long>();
+
+                        for (var j = 0; j < line[i].Count(); j++)
+                        {
+                            //takes the second half
+                            if (j >= line[i].Count() / 2)
+                            {
+                                long firstNum = 0;
+                                firstNum = line[i][j];
+
+                                if(firstNum != 0 || (j + 1) == line[i].Count() || newList.Count > 0)
+                                {
+                                    //adds second half to a new list
+                                    newList.Add(line[i][j]);
+                                }
+                            }
+                            else
+                            {
+                                firstHalfOfList.Add(line[i][j]);
+                            }
+                        }
+
+                        line[i] = firstHalfOfList;
+                        line.Insert(i + 1, newList);
+
+                        //increases index by 1 bec one is added
+                        i++;
+                    }
+
+                    //multiply by 2024
+                    else
+                    {
+                        long calcedNum = long.Parse(string.Join("", line[i])) * 2024;
+
+                        List<long> newNum = new List<long>();
+
+                        string stringed = calcedNum.ToString();
+
+                        for (var j = 0; j < stringed.Length; j++)
+                        {
+                            newNum.Add(long.Parse(stringed[j].ToString()));
+                        }
+                        line[i] = newNum;
+                    }
                 }
+                Console.WriteLine("k -----------" + k);
             }
 
-
-
-
-
             Console.WriteLine();
-            Console.WriteLine();
+            // printLine(line);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Amount of stones: " + line.Count());
+            Console.ResetColor();
+            //Answer part 1: 203457
+        }
+
+        public static void printLine(List<List<long>> line)
+        {
             //writes the list
             for (var i = 0; i < line.Count; i++)
             {
--- AdventOfCode/obj/Debug/net8.0/AdventOfCode.AssemblyInfo.cs
+++ AdventOfCode/obj/Debug/net8.0/AdventOfCode.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdventOfCode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1eb98118bbcf7920e6f5fc61363a81fdaf7c7691")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe8a7d9a43f1e05946875e45ae2d298723d658cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdventOfCode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdventOfCode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
